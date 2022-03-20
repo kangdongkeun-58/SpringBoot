a2b1461a27761b943693069d99215611dc261738
username <user@email.mail>
2022-03-20 10:11:11 +0900

    commit test

--- src/main/java/com/planing/DKDEV/Handler/ApiHandler.java
+++ src/main/java/com/planing/DKDEV/Handler/ApiHandler.java
@@ -10,6 +10,6 @@ public class ApiHandler
     @GetMapping("api/check")
     public ResponseEntity<?> apiCheck()
     {
-        return ResponseEntity.ok("Hi하이?");
+        return ResponseEntity.ok("Hello???");
     }
 }
